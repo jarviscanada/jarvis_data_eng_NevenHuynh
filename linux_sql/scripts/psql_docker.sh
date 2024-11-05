286b4d38e55910c58336f8838f029c2b5f822dcf
Neven Huynh <nevenhuynh267@gmail.com>
2024-11-05 23:41:27 +0000

    jrvs-psql script

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -6,7 +6,7 @@ db_username=$2
 db_password=$3
 
 # Start docker
-# Make sure you understand the double pipe operator
+# Checks the status of docker and if is it not running, then start docker
 sudo systemctl status docker || sudo systemctl start docker
 
 # Check container status (try the following cmds on terminal)
@@ -33,7 +33,7 @@ case $cmd in
 	docker volume create pgdata
   # Start the container
 	docker run --name jrvs-psql -e POSTGRES_USER=$db_username -e POSTGRES_PASSWORD=$db_password  -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
-  # Make sure you understand what's `$?`
+  # If docker run succeeds, then exit with error code 0. Otherwise, exit with error code 1
 	exit $?
 	;;
 

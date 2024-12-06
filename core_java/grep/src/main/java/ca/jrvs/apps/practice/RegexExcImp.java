d94f0d0de2e8fe0d7b2bdf23960000eb30588d75
Neven Huynh <nevenhuynh267@gmail.com>
2024-12-06 12:59:03 -0500

    regex practice

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -1,4 +1,36 @@
 package ca.jrvs.apps.practice;
 
-public class RegexExcImp {
+import java.util.regex.Pattern;
+
+public class RegexExcImp implements RegexExc{
+
+  @Override
+  public boolean matchJpeg(String filename) {
+    Pattern jpegMatch = Pattern.compile(".*\\.jpe?g$", Pattern.CASE_INSENSITIVE);
+    return jpegMatch.matcher(filename).matches();
+  }
+
+  @Override
+  public boolean matchIp(String ip) {
+    Pattern ipMatch = Pattern.compile("^([0-9]{1,3}.){3}[0-9]{1,3}$");
+    return ipMatch.matcher(ip).matches();
+  }
+
+  @Override
+  public boolean isEmptyLine(String line) {
+    Pattern emptyLineMatch = Pattern.compile("^\\s*$");
+    return emptyLineMatch.matcher(line).matches();
+  }
+
+  public static void main(String[] args){
+    RegexExcImp regex = new RegexExcImp();
+    System.out.println("Test if 'test.jpeg' is a jpg file: "+regex.matchJpeg("test.jpeg")); //true
+    System.out.println("Test if 'test5.JPG' is a jpg file: "+regex.matchJpeg("test5.JPG")); //true
+
+    System.out.println("\nTest if '111.2.23.9' is a valid IP: "+regex.matchIp("111.2.23.9")); //true
+    System.out.println("Test if '111.2.23.9' is a valid IP: "+regex.matchIp("1111.2.23.9")); //false
+
+    System.out.println("\nTest if ' ' is an empty space: "+regex.isEmptyLine(" ")); //true
+    System.out.println("Test if 'false' is an empty space: "+regex.isEmptyLine("false")); //false
+  }
 }
